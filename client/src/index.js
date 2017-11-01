a0654294392204f0f1b1ad06d2ba6893d7f62cb1
Arnaud Busson <abusson@civiliz.fr>
2017-11-01 18:59:17 +0100

    Add firebase

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as config from './build.properties.js';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: config.FIREBASE_API_KEY,
+    authDomain: config.FIREBASE_AUTH_DOMAIN,
+    projectId : config.FIREBASE_PROJECT_ID,
+    databaseURL: config.FIREBASE_DATABASE_URL,
+    storageBucket: config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+};
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+console.log(firebase);
