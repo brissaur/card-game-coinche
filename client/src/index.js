44170ec0901c8e875f77a7706ad2e2836fd54aa2
brissaur <robin.brissaud@matters.tech>
2017-11-01 18:59:07 +0100

    add bootstrap redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import mainReducer from './reducers'
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(mainReducer)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
