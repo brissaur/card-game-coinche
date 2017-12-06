197eaefce69223695169eae44368a673a17a6f54
brissaur <robin.brissaud@matters.tech>
2017-12-06 21:23:11 +0100

    create table on init app

--- client/src/bootstrap/sagas.js
+++ client/src/bootstrap/sagas.js
@@ -1,23 +1,34 @@
-import { put } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
 
 import db from '../api/init';
 import { setUserName } from '../users/duck';
 import { setAppLoaded } from '../bootstrap/duck';
 
+function* registerUser() {
+    // eslint-disable-next-line no-alert
+    const name = prompt('Hey! What is your name?');
+
+    if (!name) {
+        throw new Error('You need to be logged in to access coinche games.');
+    }
+
+    const document = yield db.collection('users').add({
+        firstname: name,
+    });
+
+    yield put(setUserName(name, document.id));
+    yield put(setAppLoaded());
+}
+
+function* createTable() {
+    // TODO: ARNAUD
+}
+
 export default function* initAppSagas() {
     try {
-        // eslint-disable-next-line no-alert
-        const name = prompt('Hey! What is your name?');
+        yield call(registerUser);
+        yield call(createTable);
 
-        if (!name) {
-            throw new Error('You need to be logged in to access coinche games.');
-        }
-
-        const document = yield db.collection('users').add({
-            firstname: name,
-        });
-        console.log(document)
-        yield put(setUserName(name, document.id));
         yield put(setAppLoaded());
     } catch (e) {
         yield put(setAppLoaded(e.message));
