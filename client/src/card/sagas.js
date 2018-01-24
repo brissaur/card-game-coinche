ac17a0707778eeed2ef8fcba831989eef996ecf8
brissaur <robin.brissaud@matters.tech>
2018-01-24 21:37:04 +0100

    Change trick data to object with player and card ids

--- client/src/card/sagas.js
+++ client/src/card/sagas.js
@@ -3,16 +3,23 @@ import db from '../api/init';
 
 import { CARD_PLAYED } from './ducks';
 import { getTableId } from '../table/selectors';
+import { getPlayerId } from '../player/selectors';
 
 function* cardPlayed({ card }) {
     global.console.log('New card played', card);
     const tableId = yield select(getTableId);
     const document = yield db.collection('tables').doc(tableId);
 
+    const myPlayerId = yield select(getPlayerId);
     const snap = yield document.get();
     yield document.update({
-        trick: [...snap.data().trick, card],
-        // TODO: add an object with card.id && player.id
+        trick: [
+            ...snap.data().trick,
+            {
+                playerId: myPlayerId,
+                cardId: card.id,
+            },
+        ],
     });
 }
 
