a4f8baad6ae1dae2c6187bdb02fa6b014268a88b
brissaur <robin.brissaud@matters.tech>
2018-02-14 21:34:45 +0100

    fix lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-    "extends": "airbnb",
-    "env": { "browser": true, "jest": true },
-    "rules": {
-        "no-console": 1,
-        "no-multiple-empty-lines": [2, { "max": 1, "maxEOF": 1, "maxBOF": 0 }],
-        "newline-before-return": 2,
-        "react/jsx-filename-extension": 0,
-        "react/require-default-props": 0,
-		"indent": ["error", 4],
-        "react/jsx-indent": [1, 4],
-        "react/jsx-indent-props": [1, 4],
-        "no-underscore-dangle": [2],
-        "max-len": [2, {"ignoreComments": true}],
-        "jsx-a11y/click-events-have-key-events": 0,
-        "jsx-a11y/no-static-element-interactions": 0
-    }
+    extends: 'airbnb',
+    env: { browser: true, jest: true },
+    rules: {
+        'no-multiple-empty-lines': [2, { max: 1, maxEOF: 1, maxBOF: 0 }],
+        'newline-before-return': 2,
+        'react/jsx-filename-extension': 0,
+        'react/require-default-props': 0,
+        indent: ['error', 4],
+        'react/jsx-indent': [1, 4],
+        'react/jsx-indent-props': [1, 4],
+        'no-underscore-dangle': [2],
+        'max-len': [2, { ignoreComments: true, code: 150 }],
+        'jsx-a11y/click-events-have-key-events': 0,
+        'jsx-a11y/no-static-element-interactions': 0,
+        'no-mixed-operators': 0,
+        'prefer-destructuring': 0,
+    },
 };
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,6 +1,6 @@
-import shuffle from "lodash/shuffle";
-import sortBy from "lodash/sortBy";
-import { firestore } from "firebase-functions";
+import shuffle from 'lodash/shuffle';
+import sortBy from 'lodash/sortBy';
+import { firestore } from 'firebase-functions';
 
 const assignCardsToPlayer = (cards, player) => ({ ...player, cards });
 
@@ -8,130 +8,117 @@ const assignCardsToPlayer = (cards, player) => ({ ...player, cards });
  * dataProvider { players: [ { id: 'IEOCmi6TkPJ2G0LEUInH' }, { id: 'Qc9YMPbs9qY9a6NbzofK' }, { id: 'XbPRUknEfzeVCpOuTrRA' }, { id: '3MdqHPfrUOlLK38XZKR1' }]}
  * @type {CloudFunction<DeltaDocumentSnapshot>|*}
  */
-exports.dealCards = firestore.document("tables/{tableId}").onCreate(event => {
+exports.dealCards = firestore.document('tables/{tableId}').onCreate((event) => {
     const { players } = event.data.data();
     const cards = shuffle([
-        "7S",
-        "8S",
-        "9S",
-        "10S",
-        "AS",
-        "JS",
-        "QS",
-        "KS",
-        "7H",
-        "8H",
-        "9H",
-        "10H",
-        "AH",
-        "JH",
-        "QH",
-        "KH",
-        "7D",
-        "8D",
-        "9D",
-        "10D",
-        "AD",
-        "JD",
-        "QD",
-        "KD",
-        "7C",
-        "8C",
-        "9C",
-        "10C",
-        "AC",
-        "JC",
-        "QC",
-        "KC"
+        '7S',
+        '8S',
+        '9S',
+        '10S',
+        'AS',
+        'JS',
+        'QS',
+        'KS',
+        '7H',
+        '8H',
+        '9H',
+        '10H',
+        'AH',
+        'JH',
+        'QH',
+        'KH',
+        '7D',
+        '8D',
+        '9D',
+        '10D',
+        'AD',
+        'JD',
+        'QD',
+        'KD',
+        '7C',
+        '8C',
+        '9C',
+        '10C',
+        'AC',
+        'JC',
+        'QC',
+        'KC',
     ]);
 
     for (let playerNumber = 0; playerNumber < 4; playerNumber += 1) {
-        players[playerNumber] = assignCardsToPlayer(
-            cards.slice(playerNumber * 8, playerNumber * 8 + 8),
-            players[playerNumber]
-        );
+        players[playerNumber] = assignCardsToPlayer(cards.slice(playerNumber * 8, playerNumber * 8 + 8), players[playerNumber]);
     }
 
     return event.data.ref.update({
         players,
         general: {
-            currentPlayerId: players[0].id
-        }
+            currentPlayerId: players[0].id,
+        },
     });
 });
 
-exports.giveHandNextPlayer = firestore
-    .document("tables/{tableId}")
-    .onUpdate(event => {
-        const currentData = event.data.data();
-        const previousData = event.data.previous.data();
-        const currentTrick = currentData.trick;
-        const previousTrick = previousData.trick;
-        const previousPlayers = sortBy(previousData.players, ["pos"]);
+exports.giveHandNextPlayer = firestore.document('tables/{tableId}').onUpdate((event) => {
+    const currentData = event.data.data();
+    const previousData = event.data.previous.data();
+    const currentTrick = currentData.trick;
+    const previousTrick = previousData.trick;
+    const previousPlayers = sortBy(previousData.players, ['pos']);
 
-        // Trick has changed, we need to select the next player
-        if (currentTrick.length > previousTrick.length) {
-            const previousPlayerId = previousData.general.currentPlayerId;
-            const previousPlayerIdx = previousPlayers.findIndex(
-                player => previousPlayerId === player.id
-            );
-            // if previousPlayer was the last one, take the first one
-            const currentPlayerId =
-                previousPlayers[(previousPlayerIdx + 1) % 4].id;
+    // Trick has changed, we need to select the next player
+    if (currentTrick.length > previousTrick.length) {
+        const previousPlayerId = previousData.general.currentPlayerId;
+        const previousPlayerIdx = previousPlayers.findIndex(player => previousPlayerId === player.id);
+        // if previousPlayer was the last one, take the first one
+        const currentPlayerId = previousPlayers[(previousPlayerIdx + 1) % 4].id;
 
-            return event.data.ref.set(
-                {
-                    general: {
-                        ...currentData.general,
-                        currentPlayerId
-                    }
+        return event.data.ref.set(
+            {
+                general: {
+                    ...currentData.general,
+                    currentPlayerId,
                 },
-                { merge: true }
-            );
-        }
-
-        return event;
-    });
-
-exports.fakePlayerTurn = firestore
-    .document("tables/{tableId}")
-    .onUpdate(event => {
-        const data = event.data.data();
-        const previousData = event.data.previous.data();
-        const targetPlayerId = data.general.currentPlayerId;
-
-        const hasCurrentPlayerIdChanged =
-            targetPlayerId !== previousData.general.currentPlayerId;
-        const targetPlayer = data.players.find(
-            player => player.id === targetPlayerId
+            },
+            { merge: true },
         );
+    }
 
-        const isFakePlayer = targetPlayer.isFakePlayer;
+    return event;
+});
 
-        if (hasCurrentPlayerIdChanged && isFakePlayer) {
-            const card = targetPlayer.cards[0];
-            const newHand = targetPlayer.cards.slice(1);
+exports.fakePlayerTurn = firestore.document('tables/{tableId}').onUpdate((event) => {
+    const data = event.data.data();
+    const previousData = event.data.previous.data();
+    const targetPlayerId = data.general.currentPlayerId;
 
-            return event.data.ref.set(
-                {
-                    trick: [
-                        ...data.trick,
-                        {
-                            playerId: targetPlayerId,
-                            cardId: card.id
-                        }
-                    ],
-                    players: [
-                        ...data.players.filter(
-                            ({ id }) => id !== targetPlayerId
-                        ),
-                        {
-                            id: targetPlayerId,
-                            cards: newHand
-                        }
-                    ]
-                },
-                { merge: true }
-            );
-        }
-    });
+    const hasCurrentPlayerIdChanged = targetPlayerId !== previousData.general.currentPlayerId;
+    const targetPlayer = data.players.find(player => player.id === targetPlayerId);
+
+    const isFakePlayer = targetPlayer.isFakePlayer;
+
+    if (hasCurrentPlayerIdChanged && isFakePlayer) {
+        const card = targetPlayer.cards[0];
+        const newHand = targetPlayer.cards.slice(1);
+
+        return event.data.ref.set(
+            {
+                trick: [
+                    ...data.trick,
+                    {
+                        playerId: targetPlayerId,
+                        cardId: card.id,
+                    },
+                ],
+                players: [
+                    ...data.players.filter(({ id }) => id !== targetPlayerId),
+                    {
+                        id: targetPlayerId,
+                        cards: newHand,
+                    },
+                ],
+            },
+            { merge: true },
+        );
+    }
+
+    return event;
+});
