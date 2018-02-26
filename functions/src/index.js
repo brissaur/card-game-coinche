9b433d1dd9a5faf5f83adf7c01ed64806f2b8c0e
Arnaud Busson <abusson@civiliz.fr>
2018-02-26 20:43:07 +0100

    Refacto function

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,45 +1,80 @@
 import shuffle from 'lodash/shuffle';
 import sortBy from 'lodash/sortBy';
-import { firestore } from 'firebase-functions';
+import * as functions  from 'firebase-functions';
+import * as admin from 'firebase-admin';
+//import firebase from "firebase";
+
+admin.initializeApp(functions.config().firebase);
+
+//console.log(admin.database());
 
 const assignCardsToPlayer = (cards, player) => ({ ...player, cards });
 
-function onDealCards(event) {
+const cards = shuffle([
+    '7S',
+    '8S',
+    '9S',
+    '10S',
+    'AS',
+    'JS',
+    'QS',
+    'KS',
+    '7H',
+    '8H',
+    '9H',
+    '10H',
+    'AH',
+    'JH',
+    'QH',
+    'KH',
+    '7D',
+    '8D',
+    '9D',
+    '10D',
+    'AD',
+    'JD',
+    'QD',
+    'KD',
+    '7C',
+    '8C',
+    '9C',
+    '10C',
+    'AC',
+    'JC',
+    'QC',
+    'KC',
+]);
+
+async function onDealCards(event) {
+
+    var playersRef = admin.firestore().collection('tables').doc('test').collection('players');
+    let allPlayers = [];
+    await playersRef.get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            allPlayers.push(doc.id);
+            //console.log(doc.id, '=>', doc.data());
+        });
+    })
+    .catch(err => {
+        console.log('Error getting documents', err);
+    });
+
+    console.log(allPlayers);
+
+    return event;
+
+    console.log(admin);
+
+    const result = await admin.collection('tables').doc('test').getCollections().then(collections => {
+        collections.forEach(collection => {
+            console.log('Found subcollection with id:', collection.id);
+        });
+    });
+
+    return result;
+
     const { players } = event.data.data();
-    const cards = shuffle([
-        '7S',
-        '8S',
-        '9S',
-        '10S',
-        'AS',
-        'JS',
-        'QS',
-        'KS',
-        '7H',
-        '8H',
-        '9H',
-        '10H',
-        'AH',
-        'JH',
-        'QH',
-        'KH',
-        '7D',
-        '8D',
-        '9D',
-        '10D',
-        'AD',
-        'JD',
-        'QD',
-        'KD',
-        '7C',
-        '8C',
-        '9C',
-        '10C',
-        'AC',
-        'JC',
-        'QC',
-        'KC',
-    ]);
 
     for (let playerNumber = 0; playerNumber < 4; playerNumber += 1) {
         players[playerNumber] = assignCardsToPlayer(cards.slice(playerNumber * 8, playerNumber * 8 + 8), players[playerNumber]);
@@ -114,16 +149,17 @@ function onCardPlayed(eventData) {
 }
 
 /**
+ * dataprovider { id: 'IEOCmi6TkPJ2G0LEUInH' }
  * dataProvider { players: [ { id: 'IEOCmi6TkPJ2G0LEUInH' }, { id: 'Qc9YMPbs9qY9a6NbzofK' }, { id: 'XbPRUknEfzeVCpOuTrRA' }, { id: '3MdqHPfrUOlLK38XZKR1' }]}
  * @type {CloudFunction<DeltaDocumentSnapshot>|*}
  */
-exports.dealCards = firestore.document('tables/{tableId}').onCreate(onDealCards);
+exports.dealCards = functions.firestore.document('tables/{tableId}/players').onCreate(onDealCards);
 
 /**
  * onUpdate({before: { trick: [], general: { currentPlayerId: 1 }, players: [{ id: 1}, { id: 2, isFakePlayer: true, cards: [{ id: 'AH' }, { id: '7H' }]}]},after: { trick: [{}], general: { currentPlayerId: 2 }, players: [{ id: 1}, { id: 2, isFakePlayer: true, cards: [{ id: 'AH' }, { id: '7H' }]}]}})
  *
  */
-exports.onUpdate = firestore.document('tables/{tableId}').onUpdate((event) => {
+exports.onUpdate = functions.firestore.document('tables/{tableId}').onUpdate((event) => {
     const mutations = [onFakePlayerTurn, onCardPlayed].map(f => f(event.data)).reduce((mutation, res) => ({
         ...res,
         ...mutation,
