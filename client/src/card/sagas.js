39bfdc152cbbb7b38b8f316a53b74f6c8a90399c
brissaur <robin.brissaud@matters.tech>
2018-02-27 22:10:33 +0100

    Update player hand on card played

--- client/src/card/sagas.js
+++ client/src/card/sagas.js
@@ -2,36 +2,30 @@ import { takeEvery, select } from 'redux-saga/effects';
 import db from '../api/init';
 
 import { CARD_PLAYED } from './ducks';
-import { getTableId } from '../table/selectors';
+import { getTableId, getPlayerCards } from '../table/selectors';
 import { getPlayerId } from '../player/selectors';
 
 function* cardPlayed({ card }) {
     global.console.log('New card played', card);
     const tableId = yield select(getTableId);
-    const document = yield db.collection('tables').doc(tableId);
-
     const myPlayerId = yield select(getPlayerId);
-    const snap = yield document.get();
-    const data = snap.data();
-    const me = data.players.find(player => player.id === myPlayerId);
-    const newHand = me.cards.filter(id => id !== card.id);
+    const myHand = yield select(getPlayerCards);
 
-    yield document.update({
-        trick: [
-            ...data.trick,
-            {
-                playerId: myPlayerId,
-                cardId: card.id,
-            },
-        ],
-        players: [
-            ...data.players.filter(({ id }) => id !== myPlayerId),
-            {
-                id: myPlayerId,
-                cards: newHand,
-            },
-        ],
-    });
+    yield db
+        .collection('tables')
+        .doc(tableId)
+        .collection('tricks')
+        .add({
+            playerId: myPlayerId,
+            cardId: card.id,
+        });
+
+    yield db
+        .collection('tables')
+        .doc(tableId)
+        .collection('players')
+        .doc(myPlayerId)
+        .update({ cards: myHand.filter(cardId => cardId !== card.id) });
 }
 
 // eslint-disable-next-line import/prefer-default-export
