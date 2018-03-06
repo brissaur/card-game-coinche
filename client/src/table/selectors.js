ced0f6a4f0cb5cefffb528f0ff408dfcde249846
brissaur <robin.brissaud@matters.tech>
2018-03-06 19:58:15 +0100

    display cards and sort player

--- client/src/table/selectors.js
+++ client/src/table/selectors.js
@@ -11,11 +11,20 @@ export function getTrick(state) {
     return get(state, 'table.trick');
 }
 
-export function getPlayers(state) {
+export function getPlayersRaw(state) {
     return get(state, 'table.players');
 }
 
-export const getMyPlayer = createSelector(getPlayers, getPlayerId, (players, myPlayerId) =>
+// eslint-disable-next-line no-nested-ternary
+const byPos = (a, b) => (a.pos === b.pos ? 0 : a.pos > b.pos ? 1 : -1);
+
+export const getPlayers = createSelector(getPlayersRaw, getTrick, (players, trick) =>
+    players.sort(byPos).map(player => ({
+        ...player,
+        cardPlayed: (trick.find(card => card.playerId === player.id) || {}).cardId,
+    })));
+
+export const getMyPlayer = createSelector(getPlayersRaw, getPlayerId, (players, myPlayerId) =>
     players.find(({ id }) => id === myPlayerId));
 
 export const getPlayerCards = createSelector(getMyPlayer, me => (me && me.cards ? me.cards : []));
