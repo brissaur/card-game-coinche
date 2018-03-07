a8ff63ea76f4dd190d6ba49cdeae65b3cb3c318c
Arnaud Busson <arnaud.busson@inovia-team.com>
2018-03-07 09:00:58 +0100

    WIP new round

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,3 +1,4 @@
 export const TABLE_COLLECTION = 'tables';
 export const PLAYERS_COLLECTION = 'players';
-export const TRICK_COLLECTION = 'trick';
+export const TRICK_COLLECTION = 'tricks';
+export const CARD_PLAYED_COLLECTION = 'cardsPlayed';
