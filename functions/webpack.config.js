2566a3e79abeb04003b4328854952cdfbedd6c5c
Arnaud Busson <arnaud.busson@inovia-team.com>
2018-03-08 14:11:30 +0100

    Trying

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: [
+        'babel-polyfill',
         './src/players/index.js',
         './src/tables/index.js',
         //'./src/tricks/index.js',
@@ -9,20 +10,17 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'index.js',
+        libraryTarget: 'commonjs'
     },
+    target: 'node',
     externals: ['firebase-functions', 'firebase-admin'],
     module: {
         rules: [
             {
                 test: /^(?!.*\.spec\.js$).*\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
+                use: 'babel-loader',
+                exclude: /node_modules/
             }
         ]
     }
