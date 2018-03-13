538b6a621bcde16901fc020c60175e5993ddbf45
Arnaud Busson <arnaud.busson@inovia-team.com>
2018-03-13 19:30:00 +0100

    Resolve conflict

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -3,14 +3,7 @@ const path = require('path');
 module.exports = {
     entry: [
         'babel-polyfill',
-<<<<<<< HEAD
         './src/index.js'
-=======
-        './src/players/index.js',
-        './src/tables/index.js',
-        //'./src/tricks/index.js',
-        './src/index.js',
->>>>>>> 16977a009a549c45c282ecf03e1df17bfa10ef64
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,14 +11,10 @@ module.exports = {
         libraryTarget: 'commonjs'
     },
     target: 'node',
-<<<<<<< HEAD
     externals:[
         'firebase-functions',
         'firebase-admin'
     ],
-=======
-    externals: ['firebase-functions', 'firebase-admin'],
->>>>>>> 16977a009a549c45c282ecf03e1df17bfa10ef64
     module: {
         rules: [
             {
