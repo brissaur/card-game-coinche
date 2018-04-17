ea4d0bec3a117229f7dea7571c14ec4aecbe9de9
Arnaud Busson <arnaud.busson@inovia-team.com>
2018-04-17 22:38:15 +0200

    Playable cards

--- functions/src/lib/index.spec.js
+++ functions/src/lib/index.spec.js
@@ -1,6 +1,8 @@
-import { filterCardsByColor, getCardColor } from './index';
+import { filterCardsByColor, getCardColor, sortCards, Card, Hand, getHigherCards } from './index';
 
-const cards = [
+// remove lodash, it should not be useful
+
+export const cards = [
     '7S',
     '8S',
     '9S',
@@ -35,36 +37,172 @@ const cards = [
     'KC',
 ];
 
-describe.only('test filterCardsByColor function', () => {
+describe('test function', () => {
     [
-        { card: 'AH', expected: 'H' },
-        { card: '10H', expected: 'H' },
-        { card: 'AS', expected: 'S' },
-        { card: 'JD', expected: 'D' },
-        { card: '7C', expected: 'C' },
+        { cardId: '7H', expected: { color: 'H', height: '7' } },
+        { cardId: '10H', expected: { color: 'H', height: '10' } },
+        { cardId: 'AH', expected: { color: 'H', height: 'AH' } },
     ].forEach((data) => {
-        test('getCardColor', () => {
-            expect(getCardColor(data.card)).toEqual(data.expected);
+        test('create Card', () => {
+            const card = new Card(data.cardId);
+            expect(card.color).toBe(data.expected.color);
+            expect(card.height).toBe(data.expected.height);
         });
     });
 
     [
         {
-            color: 'H',
-            expected: [
-                '7H',
-                '8H',
-                '9H',
-                '10H',
-                'AH',
-                'JH',
-                'QH',
-                'KH',
+            trump: null,
+            cards: [
+                new Card('7H'),
+                new Card('KH'),
+                new Card('JH'),
+                new Card('AH'),
+                new Card('9H'),
+                new Card('8H'),
+                new Card('10H'),
+                new Card('QH'),
             ],
+            expected: ['AH', '10H', 'KH', 'QH', 'JH', '9H', '8H', '7H'],
+        },
+        {
+            trump: 'H',
+            cards: [
+                new Card('7H'),
+                new Card('KH'),
+                new Card('JH'),
+                new Card('AH'),
+                new Card('9H'),
+                new Card('8H'),
+                new Card('10H'),
+                new Card('QH'),
+            ],
+            expected: ['JH', '9H', 'AH', '10H', 'KH', 'QH', '8H', '7H'],
         },
     ].forEach((data) => {
-        test.only('filterCardsByColor', () => {
-            expect(cards.filter(filterCardsByColor(data.color))).toEqual(data.expected);
+        test('sortCard', () => {
+            expect(data.cards.sort(sortCards(data.trump)).map(card => card.id)).toEqual(data.expected);
+        });
+    });
+
+
+    [
+        {
+            isTrump: true, // diamonds
+            eligibleCardsInHand: [
+                new Card('AD'),
+            ],
+            playedCardsInTrick: [
+                new Card('8D'),
+                new Card('10D'),
+            ],
+            expected: [
+                new Card('AD'),
+                // Card order when  `no-trumps`:
+                // A > 10 > K > Q > J > 9 > 8 > 7
+                // 11 / 10 / 4 / 3 / 2 / 0 / 0 / 0
+                //
+                // Card order in `trumps`:
+                // J > 9 > A > 10 > K > Q > 8 > 7
+                // 20 / 14 / 11 / 10 / 4 / 3 / 0 / 0
+            ],
+        },
+        // {
+        //     hand: [
+        //         new Card('KC'),
+        //         new Card('9S'),
+        //         new Card('8C'),
+        //         new Card('KH'),
+        //         new Card('AD'),
+        //         new Card('QC'),
+        //         new Card('QH'),
+        //         new Card('KS'),
+        //     ],
+        // }
+    ].forEach((data) => {
+        test.only('getHigherCard', () => {
+            expect(data.eligibleCardsInHand.filter(getHigherCards(data.isTrump, data.playedCardsInTrick)).map(card => card.id)).toEqual(data.expected.map(card => card.id));
+            // en entrée j'ai un tableau de Card a comparé
+            // je passe en paramètre la main du joueur
+
+            // en fonction de si je suis en mode "trump" ou non
+            // je dois trouver quel sont les cartes du joueurs qui sont supérieures aux cartes à comparer
+
+            // si je tri les cartes du joueur, et que je tri les cartes à comparer, ca doit être possible
+
+        });
+    });
+});
+
+describe('test Hand', () => {
+    [
+        {
+            handCards: [
+                new Card('9D'),
+                new Card('7D'),
+                new Card('10S'),
+                new Card('QD'),
+                new Card('10C'),
+                new Card('8H'),
+                new Card('9S'),
+                new Card('JC'),
+            ],
+            trump: 'H',
+            firstCardColor: 'H',
+            expected: {
+                colorCards: [
+                    new Card('8H'),
+                ],
+                trumpCards: [
+                    new Card('8H'),
+                ],
+                otherCards: [
+                    new Card('9D'),
+                    new Card('7D'),
+                    new Card('10S'),
+                    new Card('QD'),
+                    new Card('10C'),
+                    new Card('9S'),
+                    new Card('JC'),
+                ],
+            },
+        },
+        {
+            handCards: [
+                new Card('9D'),
+                new Card('7D'),
+                new Card('10S'),
+                new Card('QD'),
+                new Card('10C'),
+                new Card('8H'),
+                new Card('9S'),
+                new Card('JC'),
+            ],
+            trump: 'S',
+            firstCardColor: 'H',
+            expected: {
+                colorCards: [
+                    new Card('8H'),
+                ],
+                trumpCards: [
+                    new Card('10S'),
+                    new Card('9S'),
+                ],
+                otherCards: [
+                    new Card('9D'),
+                    new Card('7D'),
+                    new Card('QD'),
+                    new Card('10C'),
+                    new Card('JC'),
+                ],
+            },
+        },
+    ].forEach((data) => {
+        test('create Hand', () => {
+            const hand = new Hand(data.handCards, data.trump, data.firstCardColor);
+            expect(hand.getColorCards().map(card => card.id)).toEqual(data.expected.colorCards.map(card => card.id));
+            expect(hand.getTrumpCards().map(card => card.id)).toEqual(data.expected.trumpCards.map(card => card.id));
+            expect(hand.getOtherCards().map(card => card.id)).toEqual(data.expected.otherCards.map(card => card.id));
         });
     });
 });
@@ -104,7 +242,7 @@ describe.only('test filterCardsByColor function', () => {
  * Color: Couleur définie par la carte désigné au début du trick
  * Obligé de couper (joué de l'atout - ou à défaut une autre carte) quand le joueur n'a pas la couleur ou que le partenaire n'est pas en train de gagner
  */
-describe.only('test function', () => {
+describe('test function', () => {
     [
         {
             data: {
@@ -114,7 +252,7 @@ describe.only('test function', () => {
                 currentPlayerId: 2,
                 hand: ['7C', '8C', '9C'],
             },
-            expected: [],
+            expected: ['7C', '8C', '9C'],
         },
     ].forEach((data) => {
         test('computeNextPlayerAfterTrick', () => {
