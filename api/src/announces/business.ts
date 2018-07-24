ae0d056ca6200a2088234d7221405c4c95d2e495
Arnaud Busson <arnaud.busson@inovia-team.com>
2018-07-24 22:41:39 +0200

    Typescript

--- api/src/announces/business.ts
+++ api/src/announces/business.ts
@@ -1,3 +1,5 @@
+import { IAnnounce } from "./types";
+
 export function announceIA() {
     return 'pass';
 }
@@ -6,8 +8,8 @@ export function shouldStopAnnounces(announces) {
     return announces.length === 4;
 }
 
-export function getBestAnnounce(announces) {
-    const announcesWoPass = announces.filter(a => a.announce !== 'pass');
+export function getBestAnnounce(announces: IAnnounce[]) {
+    const announcesWoPass = announces.filter((a: any) => a.announce !== 'pass');
     // @TODO: case 4 pass;
 
     return announcesWoPass[announcesWoPass.length - 1];
