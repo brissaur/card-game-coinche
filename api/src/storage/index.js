adc5bedd9841d32881f8d0a39258dcc0e308138b
xtrembaker <xtrembaker@gmail.com>
2018-07-24 19:44:58 +0200

    Create API module (#33)

---
+++ api/src/storage/index.js
@@ -0,0 +1,14 @@
+const Firestore = require('@google-cloud/firestore');
+const config = require('../build.properties');
+
+const firestore = new Firestore({
+    projectId: config.FIREBASE_PROJECT_ID,
+    keyFilename: '/Users/busson-arnaud/Sites/card-game-coinche/api/card-game-coinche-f9dad125fcc4.json',
+});
+
+// prevent warning with Date Objects which will change any soon
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+
+
+module.exports = firestore;
