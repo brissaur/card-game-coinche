896f7117cc80dc4b03efb4ae5e1ecd604886e0cb
brissaur <robin.brissaud@matters.tech>
2018-09-04 21:52:15 +0200

    Add build.properties with env variables and build step for heroku

--- api/src/build.properties.dist.ts
+++ api/src/build.properties.dist.ts
@@ -4,6 +4,6 @@ export const FIREBASE_DATABASE_URL = 'https://card-game-coinche-ad92d.firebaseio
 export const FIREBASE_PROJECT_ID = 'card-game-coinche-ad92d';
 export const FIREBASE_STORAGE_BUCKET = '';
 export const FIREBASE_MESSAGING_SENDER_ID = '125766558847';
-export const GCLOUD_PRIVATE_KEY_FILE = '/Users/busson-arnaud/Sites/card-game-coinche/api/card-game-coinche-f9dad125fcc4.json';
+export const GCLOUD_PRIVATE_KEY_FILE = './api/card-game-coinche-f9dad125fcc4.json';
 export const WEBSOCKET_HOST = 'localhost';
 export const WEBSOCKET_PORT = 8080;
---
+++ api/src/build.properties.env.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+export const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+export const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+export const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+export const GCLOUD_PRIVATE_KEY_FILE = process.env.GCLOUD_PRIVATE_KEY_FILE;
+export const WEBSOCKET_HOST = process.env.WEBSOCKET_HOST;
+export const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT;
