c836fa61dbd5b39ae0b00f372c7f5fd8a01d4547
brissaur <robin.brissaud@matters.tech>
2018-09-04 21:59:43 +0200

    Fix typescript types on process.env string to number (PORT)

--- api/src/websocket/index.ts
+++ api/src/websocket/index.ts
@@ -1,11 +1,14 @@
 import WebSocket from 'ws';
 import * as config from '../build.properties';
 
-export const connection = new WebSocket.Server({
-    port: config.WEBSOCKET_PORT
-}, () => {
-    global.console.log('Server listening for WS on port ' + config.WEBSOCKET_PORT);
-});
+export const connection = new WebSocket.Server(
+    {
+        port: +config.WEBSOCKET_PORT,
+    },
+    () => {
+        global.console.log(`Server listening for WS on port ${config.WEBSOCKET_PORT}`);
+    },
+);
 
 export const CARD_PLAYED_SERVER_WS = 'card/played';
 export const ANNOUNCE_MAKE_SERVER_WS = 'announce/make';
