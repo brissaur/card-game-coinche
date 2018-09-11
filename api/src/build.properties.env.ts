ca8c4c528bedd6db88d330cef9e9ad9da5112e05
brissaur <robin.brissaud@matters.tech>
2018-09-11 21:44:09 +0200

    populate GCLOUD_PRIVATE_KEY_FILE with GOOGLE_APPLICATION_CREDENTIALS

--- api/src/build.properties.env.ts
+++ api/src/build.properties.env.ts
@@ -4,6 +4,6 @@ export const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
 export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
 export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
 export const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
-export const GCLOUD_PRIVATE_KEY_FILE = process.env.GCLOUD_PRIVATE_KEY_FILE;
+export const GCLOUD_PRIVATE_KEY_FILE = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 export const WEBSOCKET_HOST = process.env.WEBSOCKET_HOST;
 export const PORT = process.env.PORT;
